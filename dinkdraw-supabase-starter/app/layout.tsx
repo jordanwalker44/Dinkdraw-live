929664103237dc9e814d1f2312b853fa52535ed1
jordanwalker44 <jordanwalker44@gmail.com>
2026-04-04 15:06:44 -0600

    Update layout.tsx

--- dinkdraw-supabase-starter/app/layout.tsx
+++ dinkdraw-supabase-starter/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
       </head>
       <body>{children}</body>
     </html>

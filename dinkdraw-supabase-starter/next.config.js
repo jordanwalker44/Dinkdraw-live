3822a6ac8c7d49ee85b54220b47ec2eb955e6756
jordanwalker44 <jordanwalker44@gmail.com>
2026-04-04 19:11:22 -0600

    Update next.config.js

--- dinkdraw-supabase-starter/next.config.js
+++ dinkdraw-supabase-starter/next.config.js
@@ -3,6 +3,30 @@ const withPWA = require('next-pwa')({
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https:\/\/dinkdraw\.app\/tournament\/create/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'create-page',
+        expiration: {
+          maxEntries: 1,
+          maxAgeSeconds: 0,
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/dinkdraw\.app\/.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'pages',
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 60 * 60 * 24,
+        },
+      },
+    },
+  ],
 });
 
 /** @type {import('next').NextConfig} */

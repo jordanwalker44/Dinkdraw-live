03a092113423e2681c82da81926917ba4de22a27
jordanwalker44 <jordanwalker44@gmail.com>
2026-04-30 15:58:37 -0600

    Create tournament-types.ts

---
+++ dinkdraw-supabase-starter/lib/tournament-types.ts
@@ -0,0 +1,22 @@
+export type PlayerSlot = {
+  id: string;
+  tournament_id: string;
+  slot_number: number;
+  display_name: string | null;
+  claimed_by_user_id: string | null;
+  gender: string | null;
+};
+
+export type ScheduleRow = {
+  round_number: number;
+  court_number: number | null;
+  court_label: string | null;
+  team_a_player_1_id: string | null;
+  team_a_player_2_id: string | null;
+  team_b_player_1_id: string | null;
+  team_b_player_2_id: string | null;
+  team_a_score: number | null;
+  team_b_score: number | null;
+  is_bye: boolean;
+  is_complete: boolean;
+};

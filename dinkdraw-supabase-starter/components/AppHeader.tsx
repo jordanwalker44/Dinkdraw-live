67d80f8ddd113e518ebc0146b50dfb2521e21bf1
jordanwalker44 <jordanwalker44@gmail.com>
2026-05-22 21:19:30 -0600

    Update AppHeader.tsx

--- dinkdraw-supabase-starter/components/AppHeader.tsx
+++ dinkdraw-supabase-starter/components/AppHeader.tsx
@@ -21,7 +21,7 @@ right: 0,
         style={{
           maxWidth: 720,
           margin: '0 auto',
-          padding: '34px 16px 10px 16px',
+          padding: '28px 16px 10px 16px',
         }}
       >
         <Link
